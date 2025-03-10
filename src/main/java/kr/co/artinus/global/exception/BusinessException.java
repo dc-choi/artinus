268256f494aa62377f9d0cda994810ebbf34127b
dc-choi <ddagae0805@gmail.com>
2025-03-10 23:13:51 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/kr/co/artinus/global/exception/BusinessException.java
@@ -0,0 +1,22 @@
+package kr.co.artinus.global.exception;
+
+import kr.co.artinus.global.common.message.FailHttpMessage;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BusinessException extends RuntimeException {
+    private HttpStatus status;
+    private String message;
+
+    public BusinessException(FailHttpMessage response) {
+        super(response.getMessage());
+        this.status = response.getStatus();
+        this.message = response.getMessage();
+    }
+}
---
+++ src/test/java/kr/co/artinus/ArtinusApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.artinus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtinusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
